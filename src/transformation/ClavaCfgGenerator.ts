56c4ac4a597a3a4f5ab300db84084a7cbfdcfad9
Guilherme-Soares-Sequeira <guilhermesoaressequeira@hotmail.com>
2025-08-28 21:09:25 +0100

    Added support for inline assembly nodes

--- src/transformation/ClavaCfgGenerator.ts
+++ src/transformation/ClavaCfgGenerator.ts
@@ -38,6 +38,7 @@ import {
     Program,
     ReturnStmt,
     Scope,
+    Statement,
     Switch,
     Vardecl,
     WrapperStmt,
@@ -51,6 +52,7 @@ import BaseGraph from "@specs-feup/flow/graph/BaseGraph";
 import Graph from "@specs-feup/flow/graph/Graph";
 import Node from "@specs-feup/flow/graph/Node";
 import Query from "@specs-feup/lara/api/weaver/Query.js";
+import AsmStatementNode from "../cfg/node/AsmStatementNode.js";
 
 class SubGraph {
     head: ClavaControlFlowNode.Class | undefined;
@@ -516,6 +518,9 @@ export default class ClavaCfgGenerator
             return this.#processLabel(jp.decl, ctx);
         } else if (jp instanceof GotoStmt) {
             return this.#processGoto(jp, ctx);
+        } else if (jp instanceof Statement && jp.type === undefined && (jp.code.startsWith("__asm__ ") || jp.code.startsWith("__asm__("))) {
+            const node = ctx.addCfgNode(new AsmStatementNode.Builder(jp));
+            return SubGraph.fromSingle(node);
         }
 
         throw new Error("Unsupported joinpoint type " + jp.joinPointType);
